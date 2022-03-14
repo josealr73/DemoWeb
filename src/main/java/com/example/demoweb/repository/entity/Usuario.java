7bd9be3f7890d71ba0920d7ad644067c86c4c233
Jose <33730912+josealr73@users.noreply.github.com>
2022-03-14 11:44:32 +0100

    Spring Security

--- src/main/java/com/example/demoweb/repository/entity/Usuario.java
+++ src/main/java/com/example/demoweb/repository/entity/Usuario.java
@@ -1,14 +1,20 @@
 package com.example.demoweb.repository.entity;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity
 @Table
-public class Usuario {
+public class Usuario implements UserDetails {
 	@Id
 	@Column
 	private String username;
@@ -52,5 +58,35 @@ public class Usuario {
 
 	public void setRol(Rol rol) {
 		this.rol = rol;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return Arrays.asList(this.rol);
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
 	}
 }
--- src/main/java/com/example/demoweb/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/example/demoweb/service/impl/UsuarioServiceImpl.java
@@ -3,6 +3,9 @@ package com.example.demoweb.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.example.demoweb.repository.UsuarioRepoJPA;
@@ -10,7 +13,7 @@ import com.example.demoweb.repository.entity.Usuario;
 import com.example.demoweb.service.UsuarioService;
 
 @Service
-public class UsuarioServiceImpl implements UsuarioService{
+public class UsuarioServiceImpl implements UsuarioService, UserDetailsService{
 
 	@Autowired
 	UsuarioRepoJPA usuarioDAO;
@@ -24,7 +27,13 @@ public class UsuarioServiceImpl implements UsuarioService{
 	@Override
 	public Usuario buscarPorUsername(String username) {
 		// TODO Auto-generated method stub
-		return usuarioDAO.getById(username);
+		return usuarioDAO.findById(username).get();
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return buscarPorUsername(username);
 	}
 
 }
